f565ec18e8b9af03dc26fafebf4faced93fa6876
SAKURA <70046367+Yejia995@users.noreply.github.com>
2024-04-04 01:17:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('voiceStateUpdate', (old_state, new_state) => {
   if (!old_state.channel && new_state.channel) {
     log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${new_state.channel.id}>`;
   } else if (old_state.channel && !new_state.channel) {
-    log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_left: <#${old_state.channel.id}>`;
+    log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${old_state.channel.id}> :arrow_right:`;
   } else if (old_state.channel?.id !== new_state.channel?.id) {
     log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${old_state.channel.id}> :arrow_right: <#${new_state.channel.id}>`;
   }
