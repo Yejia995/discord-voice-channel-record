ed8a6c42b0352dd54085577b6723a37b70e0dc55
SAKURA <70046367+Yejia995@users.noreply.github.com>
2024-04-04 21:37:51 -0400

    Update index.js
    Found a better emoji

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ client.on('voiceStateUpdate', (old_state, new_state) => {
   let log_message;
 
   if (!old_state.channel && new_state.channel) {
-    log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${new_state.channel.id}>`;
+    log_message = `<t:${timestamp}:T> <@${member_id}> :wheelchair: <#${new_state.channel.id}>`;
   } else if (old_state.channel && !new_state.channel) {
-    log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${old_state.channel.id}> :arrow_right:`;
+    log_message = `<t:${timestamp}:T> <@${member_id}> :wheelchair: <#${old_state.channel.id}> :wheelchair:`;
   } else if (old_state.channel?.id !== new_state.channel?.id) {
-    log_message = `<t:${timestamp}:T> <@${member_id}> :arrow_right: <#${old_state.channel.id}> :arrow_right: <#${new_state.channel.id}>`;
+    log_message = `<t:${timestamp}:T> <@${member_id}> :wheelchair: <#${old_state.channel.id}> :wheelchair: <#${new_state.channel.id}>`;
   }
 
   if (log_message) {
